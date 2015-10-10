8b2d8eba10c8ae5096e32e40206354d038c7489e
James Forcier <csssuf@csssuf.net>
2015-10-10 19:45:34 -0400

    Read kernel into correct segment

--- kernel/src/mbr.s
+++ kernel/src/mbr.s
@@ -2,7 +2,7 @@
 [ORG    0x7C00]
 
 boot:
-    mov ax, cs
+    mov ax, cs ; initialize segment registers
     mov ds, ax
     mov es, ax
     mov ax, 0
@@ -10,21 +10,22 @@ boot:
     mov sp, 0x7C00 ; stack lives at 0x7C00, grows towards 0x500
 reset_drive:
     mov ax, 0
-    mov dl, 0
+    mov dl, 0 ;drive 0
     int 0x13
-    jc reset_drive
+    jc reset_drive ;if failure, try again
 read_drive:
-    mov ax, 0x7E00
+    mov ax, 0x07E0 ; load to immediately after MBR
     mov es, ax
     mov bx, 0
-    mov ah, 2
-    mov al, 1
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov dl, 0
+
+    mov ah, 2 ; load to ES:BX
+    mov al, 1 ; load 1 sector
+    mov ch, 0 ; cylinder 0
+    mov cl, 2 ; sector 2
+    mov dh, 0 ; head 0
+    mov dl, 0 ; drive 0
     int 0x13
-    jc read_drive
+    jc read_drive ; if failure, try again
 
     jmp kmain
 
