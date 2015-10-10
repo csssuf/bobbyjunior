f46e54143c34f41995f7ef41195d5f49441e6af6
James Forcier <csssuf@csssuf.net>
2015-10-10 16:57:53 -0400

    Add initial kernel, plus dir structure
    Makefile, directory structure, and initial test kernel implemented.

---
+++ kernel/src/main.s
@@ -0,0 +1,38 @@
+[BITS   16]
+[ORG    0x7C00]
+
+main:
+    mov ax, hw
+    call print_string
+    call other_entry
+    jmp $
+
+print_char:
+    push bx
+    mov ah, 0x0e
+    mov bl, 0x07
+    mov bh, 0x00
+    int 0x10
+    pop bx
+    ret
+
+print_string:
+    mov bx, ax
+    mov ch, 0
+print_string_loop:
+    mov byte al, [bx]
+    mov cl, al
+    jcxz print_string_done
+    call print_char
+    inc bl
+    jmp print_string_loop
+print_string_done:
+    ret
+
+hw: db 'Hello, world',0
+
+%include "other.s"
+
+times 510-($-$$) db 0
+db 0x55
+db 0xaa
---
+++ kernel/src/other.s
@@ -0,0 +1,6 @@
+other_entry:
+    mov ax, hw2
+    call print_string
+    ret
+
+hw2: db 0xA,'This is also a string',0
