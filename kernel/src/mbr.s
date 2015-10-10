eb17f95552d331e241f96cf12ef9116c48a816fb
James Forcier <csssuf@csssuf.net>
2015-10-10 18:43:17 -0400

    Add initial implementation of reading from drive

--- kernel/src/mbr.s
+++ kernel/src/mbr.s
@@ -2,11 +2,30 @@
 [ORG    0x7C00]
 
 boot:
-    mov ax, 0
+    mov ax, cs
     mov ds, ax
     mov es, ax
+    mov ax, 0
     mov ss, ax
-    mov sp, 0x7C00 ; stack lives at 0x1500, grows towards 0x500
+    mov sp, 0x7C00 ; stack lives at 0x7C00, grows towards 0x500
+reset_drive:
+    mov ax, 0
+    mov dl, 0
+    int 0x13
+    jc reset_drive
+read_drive:
+    mov ax, 0x7E00
+    mov es, ax
+    mov bx, 0
+    mov ah, 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0
+    int 0x13
+    jc read_drive
+
     jmp kmain
 
 times 510-($-$$) db 0
