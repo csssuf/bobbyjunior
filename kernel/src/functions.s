b17321c6bea24838138564d40c3c47ba89317a42
James Forcier <csssuf@csssuf.net>
2015-10-10 20:09:12 -0400

    Add pusha and popa
    pusha and popa push and pop, respectively, all general purpose
    registers. All 4 are callee-save, so all functions should immediately
    call pusha and call popa just before returning.

--- kernel/src/functions.s
+++ kernel/src/functions.s
@@ -1,13 +1,14 @@
 print_char:
-    push bx
+    call pusha
     mov ah, 0x0e
     mov bl, 0x07
     mov bh, 0x00
     int 0x10
-    pop bx
+    call popa
     ret
 
 print_string:
+    call pusha
     mov bx, ax
     mov ch, 0
 _print_string_loop:
@@ -18,12 +19,29 @@ _print_string_loop:
     inc bl
     jmp _print_string_loop
 _print_string_done:
+    call popa
     ret
 
 print_line:
+    call pusha
     call print_string
     mov al, 0xD
     call print_char
     mov al, 0xA
     call print_char
+    call popa
+    ret
+
+pusha:
+    push ax
+    push bx
+    push cx
+    push dx
+    ret
+
+popa:
+    pop dx
+    pop cx
+    pop bx
+    pop ax
     ret
