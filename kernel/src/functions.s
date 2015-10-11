9cf6c50ace41f534f052bd9f7c854c785e126f4b
James Forcier <csssuf@csssuf.net>
2015-10-10 20:18:14 -0400

    Revert "Add pusha and popa"
    This reverts commit b17321c6bea24838138564d40c3c47ba89317a42.

--- kernel/src/functions.s
+++ kernel/src/functions.s
@@ -1,14 +1,13 @@
 print_char:
-    call pusha
+    push bx
     mov ah, 0x0e
     mov bl, 0x07
     mov bh, 0x00
     int 0x10
-    call popa
+    pop bx
     ret
 
 print_string:
-    call pusha
     mov bx, ax
     mov ch, 0
 _print_string_loop:
@@ -19,29 +18,12 @@ _print_string_loop:
     inc bl
     jmp _print_string_loop
 _print_string_done:
-    call popa
     ret
 
 print_line:
-    call pusha
     call print_string
     mov al, 0xD
     call print_char
     mov al, 0xA
     call print_char
-    call popa
-    ret
-
-pusha:
-    push ax
-    push bx
-    push cx
-    push dx
-    ret
-
-popa:
-    pop dx
-    pop cx
-    pop bx
-    pop ax
     ret
