81febfbe60b809e9696bfb99b38b0771e3eb1e1d
James Forcier <csssuf@csssuf.net>
2015-10-11 15:36:23 -0400

    Update CSH logo to look better

--- kernel/src/main.s
+++ kernel/src/main.s
@@ -1,6 +1,6 @@
 %include "functions.s"
 %include "memory.s"
-%include "forth.s"
+;%include "forth.s"
 
 kmain:
     mov dx, hw
@@ -28,17 +28,26 @@ kmain:
     mov dx, logo7
     push dx
     call print_line
+    mov dx, logo8
+    push dx
+    call print_line
+    mov dx, logo9
+    push dx
+    call print_line
 
     add sp, 2
 
-    jmp forth_main
+    ;jmp forth_main
+    jmp $
 
 hw: db 'Kernel loaded.',0
-logo1: db '===========  =',0
-logo2: db '=  =====  =  =',0
-logo3: db '=  =      =  =',0
-logo4: db '=  =====  ====',0
-logo5: db '=      =  =  =',0
-logo6: db '=  =====  =  =',0
-logo7: db '===========  =',0
+logo1: db '=========  =',0
+logo2: db '=       =  =',0
+logo3: db '= =====    =',0
+logo4: db '= =     =  =',0
+logo5: db '= ===== ====',0
+logo6: db '=     = =  =',0
+logo7: db '= =====    =',0
+logo8: db '=       =  =',0
+logo9: db '=========  =',0
 
