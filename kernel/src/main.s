4e9a6a9dbf794c3c894d2fbb27baa512ce967e13
Ross Delinger <rossdylan@csh.rit.edu>
2015-10-11 12:59:41 -0400

    fixed formatting for the logo

--- kernel/src/main.s
+++ kernel/src/main.s
@@ -1,19 +1,44 @@
 %include "functions.s"
 %include "memory.s"
+%include "forth.s"
 
 kmain:
     mov dx, hw
     push dx
     call print_line
+
+    mov dx, logo1
+    push dx
+    call print_line
+    mov dx, logo2
+    push dx
+    call print_line
+    mov dx, logo3
+    push dx
+    call print_line
+    mov dx, logo4
+    push dx
+    call print_line
+    mov dx, logo5
+    push dx
+    call print_line
+    mov dx, logo6
+    push dx
+    call print_line
+    mov dx, logo7
+    push dx
+    call print_line
+
     add sp, 2
 
-    ; do a thing (console typing)
-    .console_loop:
-        call get_char
-        add sp, 2
-        push ax ; push result of get_char on to stack for print_char
-        call print_char
-        jmp .console_loop
-    jmp $
+    jmp forth_main
 
 hw: db 'Kernel loaded.',0
+logo1: db '===========  =',0
+logo2: db '=  =====  =  =',0
+logo3: db '=  =      =  =',0
+logo4: db '=  =====  ====',0
+logo5: db '=      =  =  =',0
+logo6: db '=  =====  =  =',0
+logo7: db '===========  =',0
+
